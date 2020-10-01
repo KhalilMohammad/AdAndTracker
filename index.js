2833526af487c12f3e9f18a5f9049dbd9c6f1099
KhalilMohammad <khalilmohammadmirza@gmail.com>
2020-10-01 20:15:15 +0500

    Use environment variable for port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { getPageAdStatistics } = require("./browserService");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
